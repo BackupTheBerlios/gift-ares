9a8d5ce76274eebef94eb43bbe76f41bb6f44c07
mkern <mkern>
2004-08-21 12:59:57 +0000

    Added test application.

---
+++ gift-ares/test/main.c
@@ -0,0 +1,18 @@
+/*
+ * $Id: main.c,v 1.1 2004/08/21 12:59:57 mkern Exp $
+ *
+ * Copyright (C) 2004 Markus Kern <mkern@users.berlios.de>
+ * Copyright (C) 2004 Tom Hargreaves <hex@freezone.co.uk>
+ *
+ * All rights reserved.
+ */
+
+#include "main.h"
+
+int main (int argc, char *argv[])
+{
+	printf ("Starting up Ares test app...\n");
+
+}
+
+
---
+++ gift-ares/test/main.h
@@ -0,0 +1,25 @@
+/*
+ * $Id: main.h,v 1.1 2004/08/21 12:59:57 mkern Exp $
+ *
+ * Copyright (C) 2004 Markus Kern <mkern@users.berlios.de>
+ * Copyright (C) 2004 Tom Hargreaves <hex@freezone.co.uk>
+ *
+ * All rights reserved.
+ */
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/*****************************************************************************/
+
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "as_ares.h"
+
+/*****************************************************************************/
+
+#endif /* __MAIN_H */
