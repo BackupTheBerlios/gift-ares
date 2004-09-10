0b390fb9b8762c8efd39669f5a30fcfa13f3af4d
mkern <mkern>
2004-09-10 18:04:46 +0000

    So far so good.

--- gift-ares/lib/as_download_1.c
+++ gift-ares/lib/as_download_1.c
@@ -1,5 +1,5 @@
 /*
- * $Id: as_download_1.c,v 1.1 2004/09/09 16:12:29 mkern Exp $
+ * $Id: as_download_1.c,v 1.2 2004/09/10 18:04:46 mkern Exp $
  *
  * Copyright (C) 2004 Markus Kern <mkern@users.berlios.de>
  * Copyright (C) 2004 Tom Hargreaves <hex@freezone.co.uk>
@@ -11,4 +11,212 @@
 
 /*****************************************************************************/
 
+/* filename prefix for incomplete files */
+#define INCOMPLETE_PREFIX "_ARESTRA_"
+
 /*****************************************************************************/
+
+static as_bool conn_state_cb (ASDownConn *conn, ASDownConnState state);
+
+static as_bool conn_data_cb (ASDownConn *conn, as_uint8 *data,
+                             unsigned int len);
+
+/*****************************************************************************/
+
+/* Create download. */
+ASDownload *as_download_create (ASDownloadStateCb state_cb)
+{
+	ASDownload *dl;
+
+	if (!(dl = malloc (sizeof (ASDownload))))
+		return NULL;
+
+	dl->hash     = NULL;
+	dl->filename = NULL;
+	dl->size     = 0;
+	dl->received = 0;
+	dl->fp       = NULL;
+
+	dl->conns  = NULL;
+	dl->chunks = NULL;
+
+	dl->state    = DOWNLOAD_NEW;
+	dl->state_cb = state_cb;
+	
+	dl->udata = NULL;
+
+	return dl;
+}
+
+/* Stop and free download. */
+void as_download_free (ASDownload *dl)
+{
+	Link *l;
+
+	if (!dl)
+		return;
+	
+	/* TODO: save state */
+
+	as_hash_free (dl->hash);
+	free (dl->filename);
+	fclose (dl->fp);
+
+	for (l = dl->conns; l; l = l->next)
+		as_downconn_free (dl->conns);
+	list_free (dl->conns);
+
+	for (l = dl->chunks; l; l = l->next)
+		as_downchunk_free (dl->chunks);
+	list_free (dl->chunks);
+
+	free (dl);
+}
+
+/*****************************************************************************/
+
+/* Start download using hash, filesize and save name. */
+as_bool as_download_start (ASDownload *dl, ASHash *hash, size_t filesize,
+                           const char *filename)
+{
+	ASDownChunk *chunk;
+
+	if (dl->state != DOWNLOAD_NEW)
+	{
+		assert (dl->state == DOWNLOAD_NEW);
+		return FALSE;
+	}
+
+	if (!hash || !filename || filesize == 0)
+	{
+		assert (hash);
+		assert (filename);
+		assert (filesize > 0);
+		return FALSE;
+	}
+
+	/* open file */
+	dl->filename = stringf_dup ("%s%s", INCOMPLETE_PREFIX, filename);
+
+	if (!(fp = fopen (dl->filename, "wb")))
+	{
+		AS_ERR_1 ("Unable to open download file \"%s\" for writing",
+		          dl->filename);
+		free (dl->filename);
+		dl->filename = NULL;
+		return FALSE;
+	}
+
+	/* create one initial chunk for entire file */
+	dl->size = filesize;
+
+	if (!(chunk = as_downchunk_create (0, dl->size)))
+	{
+		AS_ERR ("Insufficient memory");
+		free (dl->filename);
+		dl->filename = NULL;
+		dl->size = 0;
+		return FALSE;
+	}
+
+	dl->chunks = list_prepend (dl->chunks, chunk);
+
+	/* copy hash */
+	dl->hash = as_hash_copy (hash);
+
+	/* TODO: reevaluate chunking to kick things off */
+
+	return TRUE;
+}
+
+/* Restart download from incomplete _ARESTRA_ file. This will fail if the file
+ * is not found/corrupt/etc.
+ */
+as_bool as_download_restart (ASDownload *dl, const char *filename)
+{
+	/* TODO */
+	assert (0);
+	return FALSE;
+}
+
+/* Pause download */
+as_bool as_download_pause (ASDownload *dl)
+{
+	if (dl->state == DOWNLOAD_PAUSED)
+		return TRUE;
+
+	/* TODO */
+
+	return TRUE;
+}
+
+/* Resume paused download */
+as_bool as_download_resume (ASDownload *dl)
+{
+	if (dl->state != DOWNLOAD_PAUSED)
+		return TRUE;
+
+	/* TODO */
+
+	return TRUE;
+}
+
+/* Returns current download state */
+ASDownloadState as_download_state (ASDownload *dl)
+{
+	return dl->state
+}
+
+/*****************************************************************************/
+
+/* Add source to download (copies source). */
+as_bool as_download_add_source (ASDownload *dl, ASSource *source)
+{	
+	Link *l;
+	ASDownConn *conn;
+
+	/* Sources can be added in any state but we need to make sure there are
+	 * no duplicates
+	 */
+	for (l = dl->conns; l; l = l->next)
+	{
+		conn = l->data;
+		if (as_source_equal (conn->source, source))
+		{
+			AS_DBG_1 ("Source \"%s\" already added.", as_source_str (source));
+			return FALSE;
+		}
+	}
+
+	/* create new connection and add it */
+	if (!(conn = as_downconn_create (source, conn_state_cb, conn_data_cb)))
+		return FALSE;
+
+	dl->conns = list_prepend (dl->conns, conn);
+
+	/* TODO: reevaluate chunking */
+
+	return TRUE;
+}
+
+/*****************************************************************************/
+
+/* Called for every state change. Return FALSE if the connection was freed and
+ * must no longer be accessed.
+ */
+static as_bool conn_state_cb (ASDownConn *conn, ASDownConnState state)
+{
+
+}
+
+/* Called for every piece of data downloaded. Return FALSE if the connection
+ * was freed and must no longer be accessed.
+ */
+static as_bool conn_data_cb (ASDownConn *conn, as_uint8 *data,
+                             unsigned int len)
+{
+
+}
+
+/*****************************************************************************/
+
--- gift-ares/lib/as_download_1.h
+++ gift-ares/lib/as_download_1.h
@@ -1,5 +1,5 @@
 /*
- * $Id: as_download_1.h,v 1.2 2004/09/09 16:55:46 mkern Exp $
+ * $Id: as_download_1.h,v 1.3 2004/09/10 18:04:46 mkern Exp $
  *
  * Copyright (C) 2004 Markus Kern <mkern@users.berlios.de>
  * Copyright (C) 2004 Tom Hargreaves <hex@freezone.co.uk>
@@ -12,40 +12,77 @@
 
 /*****************************************************************************/
 
-typedef struct
+typedef enum
 {
+	DOWNLOAD_NEW,        /* Initial state before download is started. */
+	DOWNLOAD_ACTIVE,     /* Download is transfering/looking for sources. */
+	DOWNLOAD_QUEUED,     /* Download is locally queued (because
+	                      * AS_DOWNLOAD_MAX_ACTIVE is reached). */
+	DOWNLOAD_PAUSED,     /* Download is paused by user (or disk full). */
+	DOWNLOAD_COMPLETE,   /* Download completed successfully. */
+	DOWNLOAD_FAILED      /* Download was fully transfered but hash check
+	                      * failed. */
+} ASDownloadState;
 
+typedef struct as_download_t ASDownload;
 
-	unsigned char *filename;
-	ASSource *source;
-	ASHttpHeader *request;
-	ASHttpClient *client;
-	ASHash   *hash;
-	TCPC     *c;
-	as_uint32 size;
-	as_uint32 retrieved;
-	FILE     *f;
-	timer_id  timer;
+/* Called for every state change. Return FALSE if the download was freed and
+ * must no longer be accessed.
+ */
+typedef as_bool (*ASDownloadStateCb) (ASDownload *dl, ASDownloadState state);
 
-} ASDownload;
+typedef struct as_download_t
+{
+	ASHash *hash:      /* file hash */
+	char   *filename;  /* save file name */
+	size_t  size;      /* file size */
+	size_t  received;  /* total number of bytes already received */
+	FILE   *fp;        /* file pointer */
+
+	List *conns;       /* List of ASDownConn's with sources */
+
+	List *chunks;      /* List of chunks sorted by chunk->start and always
+	                    * kept without holes and overlap */
+
+	/* download state */
+	ASDownloadState state;
+
+	ASDownloadStateCb state_cb;
+
+	void *udata; /* arbitrary user data */
+};
 
 /*****************************************************************************/
 
-/* Create download from hash, filesize and save name. */
-ASDownload *as_download_create (ASHash *hash, size_t size,
-                                const char *filename);
+/* Create download. */
+ASDownload *as_download_create (ASDownloadStateCb state_cb);
 
-/* Create download from incomplete _ARESTRA_ file. This will fail if the file
- * is not found/corrupt/etc.
- */
-ASDownload *as_download_recreate (const char *filename);
-
-/* Free download and lose all data. */
+/* Stop and free download. */
 void as_download_free (ASDownload *dl);
 
 /*****************************************************************************/
 
-/* Add source to download. */
+/* Start download using hash, filesize and save name. */
+as_bool as_download_start (ASDownload *dl, ASHash *hash, size_t filesize,
+                           const char *filename);
+
+/* Restart download from incomplete _ARESTRA_ file. This will fail if the file
+ * is not found/corrupt/etc.
+ */
+as_bool as_download_restart (ASDownload *dl, const char *filename);
+
+/* Pause download */
+as_bool as_download_pause (ASDownload *dl);
+
+/* Resume paused download */
+as_bool as_download_resume (ASDownload *dl);
+
+/* Returns current download state */
+ASDownloadState as_download_state (ASDownload *dl);
+
+/*****************************************************************************/
+
+/* Add source to download (copies source). */
 as_bool as_download_add_source (ASDownload *dl, ASSource *source);
 
 /*****************************************************************************/
