e52b920f803f28327c36f22a7fa153a982bb1964
HEx <HEx>
2004-09-18 02:21:18 +0000

    conglobulation!

--- gift-ares/lib/as_share_man.c
+++ gift-ares/lib/as_share_man.c
@@ -1,5 +1,5 @@
 /*
- * $Id: as_share_man.c,v 1.3 2004/09/16 23:36:14 HEx Exp $
+ * $Id: as_share_man.c,v 1.4 2004/09/18 02:21:18 HEx Exp $
  *
  * Copyright (C) 2004 Markus Kern <mkern@users.berlios.de>
  * Copyright (C) 2004 Tom Hargreaves <hex@freezone.co.uk>
@@ -40,6 +40,8 @@ void as_shareman_free (ASShareMan *man)
 	free (man);
 }
 
+/***********************************************************************/
+
 as_bool as_shareman_add (ASShareMan *man, ASShare *share)
 {
 	if (!as_hashtable_insert (man->table, share->hash->data,
@@ -75,31 +77,66 @@ as_bool as_shareman_remove (ASShareMan *man, ASShare *share)
 	return TRUE;
 }
 
+/***********************************************************************/
 
-static int share_send (ASShare *share, ASSession *session)
+typedef struct
 {
-	ASPacket *p = as_share_packet (share);
-	int ret;
-	if (!p)
-		return FALSE;
+	ASSession *session;
+	ASPacket  *data;
+} Conglobulator;
 
-#if 0
-	ret =  as_session_send (session, PACKET_SHARE, p, PACKET_ENCRYPT);
-#else
-	as_packet_header (p, PACKET_SHARE);
+/* The maximum glob size after which we compress and send; note that
+ * this is only vaguely related to maximum packet size.
+ */
+#define GLOB_MAX 4096
 
-	/* FIXME: chunk these */
-	ret = as_session_send (session, PACKET_COMPRESSED, p, PACKET_COMPRESS);
-#endif
-	as_packet_free (p);
+static as_bool conglobulator_flush (Conglobulator *glob)
+{
+	int ret = as_session_send (glob->session, PACKET_COMPRESSED,
+				   glob->data, PACKET_COMPRESS);
+
+	as_packet_free (glob->data);
+	glob->data = NULL;
 
 	return ret;
 }
 
+static as_bool conglobulator_assimilate (Conglobulator *glob, ASPacket *p)
+{
+	if (!glob->data)
+		glob->data = p;
+	else
+	{
+		as_packet_append (glob->data, p);
+		as_packet_free (p);
+	}
+
+	if (glob->data->used > GLOB_MAX)
+		return conglobulator_flush (glob);
+
+	return TRUE;
+}
+
+static int share_send (ASShare *share, Conglobulator *glob)
+{
+	ASPacket *p = as_share_packet (share);
+
+	if (!p)
+		return FALSE;
+
+	as_packet_header (p, PACKET_SHARE);
+	
+	return conglobulator_assimilate (glob, p);
+}
+
 as_bool as_shareman_submit (ASShareMan *man, ASSession *session)
 {
-	/* dammit, WTF does this return void?! */
-	list_foreach (man->shares, (ListForeachFunc)share_send, session);
+	Conglobulator glob = { session, NULL };
 
+	/* dammit, WTF does this return void?! */
+	list_foreach (man->shares, (ListForeachFunc)share_send, &glob);
+
+	conglobulator_flush (&glob);
+		
 	return TRUE;
 }
