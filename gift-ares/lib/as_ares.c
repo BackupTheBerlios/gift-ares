1a692e0c12f52bc97668c425980c234c715b795f
mkern <mkern>
2004-10-10 18:55:13 +0000

    Make sure netinfo object is still around when disconnecting on shutdown.

--- gift-ares/lib/as_ares.c
+++ gift-ares/lib/as_ares.c
@@ -1,5 +1,5 @@
 /*
- * $Id: as_ares.c,v 1.14 2004/10/03 14:59:22 HEx Exp $
+ * $Id: as_ares.c,v 1.15 2004/10/10 18:55:13 mkern Exp $
  *
  * Copyright (C) 2004 Markus Kern <mkern@users.berlios.de>
  * Copyright (C) 2004 Tom Hargreaves <hex@freezone.co.uk>
@@ -42,6 +42,13 @@ as_bool as_init ()
 	AS->shareman  = NULL;
 	AS->server    = NULL;
 
+	if (!(AS->netinfo = as_netinfo_create ()))
+	{
+		AS_ERR ("Failed to create network info");
+		as_cleanup ();
+		return FALSE;
+	}
+
 	if (!(AS->nodeman = as_nodeman_create ()))
 	{
 		AS_ERR ("Failed to create node manager");
@@ -52,13 +59,6 @@ as_bool as_init ()
 	if (!(AS->sessman = as_sessman_create ()))
 	{
 		AS_ERR ("Failed to create session manager");
-		as_cleanup ();
-		return FALSE;
-	}
-
-	if (!(AS->netinfo = as_netinfo_create ()))
-	{
-		AS_ERR ("Failed to create network info");
 		as_cleanup ();
 		return FALSE;
 	}
@@ -128,10 +128,10 @@ as_bool as_cleanup ()
 	as_downman_free (AS->downman);
 	as_pushman_free (AS->pushman); /* Don't free before downman */
 	as_searchman_free (AS->searchman);
-	as_netinfo_free (AS->netinfo);
 	as_shareman_free (AS->shareman);
 	as_sessman_free (AS->sessman);
 	as_nodeman_free (AS->nodeman);
+	as_netinfo_free (AS->netinfo);
 	as_http_server_free (AS->server);
 
 	free (AS);
