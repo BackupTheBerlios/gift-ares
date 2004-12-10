95a4d87f2dc564fa2e68766d82155319ce622930
hex <hex>
2004-12-10 02:27:39 +0000

    oops

--- gift-ares/gift/asp_plugin.c
+++ gift-ares/gift/asp_plugin.c
@@ -1,5 +1,5 @@
 /*
- * $Id: asp_plugin.c,v 1.5 2004/12/10 02:23:13 hex Exp $
+ * $Id: asp_plugin.c,v 1.6 2004/12/10 02:27:39 hex Exp $
  *
  * Copyright (C) 2003 giFT-Ares project
  * http://developer.berlios.de/projects/gift-ares
@@ -70,7 +70,7 @@ static int asp_giftcb_user_cmp (Protocol *p, const char *a, const char *b)
 {
 	const char *aa = strrchr (a, '@'), *bb = strrchr (b, '@');
 
-	return strcmp ((aa ? aa : a), (bb ? bb : b));
+	return strcmp ((aa ? aa+1 : a), (bb ? bb+1 : b));
 }
 
 static int asp_giftcb_chunk_suspend (Protocol *p, Transfer *transfer,
