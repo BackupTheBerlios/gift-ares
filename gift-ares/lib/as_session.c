aca1e8ec3f3ec7e6191cf5971a42de9b9596ff06
mkern <mkern>
2005-08-01 16:01:56 +0000

    The old network is short of supernodes so take what we can get.

--- gift-ares/lib/as_session.c
+++ gift-ares/lib/as_session.c
@@ -1,5 +1,5 @@
 /*
- * $Id: as_session.c,v 1.37 2005/01/17 13:25:57 mkern Exp $
+ * $Id: as_session.c,v 1.38 2005/08/01 16:01:56 mkern Exp $
  *
  * Copyright (C) 2004 Markus Kern <mkern@users.berlios.de>
  * Copyright (C) 2004 Tom Hargreaves <hex@freezone.co.uk>
@@ -532,6 +532,7 @@ static as_bool session_handshake (ASSession *session,  ASPacketType type,
 		as_nodeman_update_reported (AS->nodeman, host, port);
 	}
 
+#if 0	
 	if (children > 350)
 	{
 		/* Ares disconnects if there are more than 350 children. Do the
@@ -543,6 +544,7 @@ static as_bool session_handshake (ASSession *session,  ASPacketType type,
 		free (supernode_guid);
 		return FALSE;
 	}
+#endif	
 
 	/* Set up cipher. */
 	as_cipher_set_seeds (session->cipher, seed_16, seed_8);
