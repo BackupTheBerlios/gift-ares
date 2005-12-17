d57b5483107eef726f3fb72a47b62eab505e6f02
mkern <mkern>
2005-12-17 23:09:58 +0000

    Added GPL notice.

--- gift-ares/tools/ares_ip2port.c
+++ gift-ares/tools/ares_ip2port.c
@@ -1,3 +1,20 @@
+/*
+ * $Id: ares_ip2port.c,v 1.5 2005/12/17 23:09:58 mkern Exp $
+ *
+ * Copyright (C) 2003 giFT-Ares project
+ * http://developer.berlios.de/projects/gift-ares
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
--- gift-ares/tools/gift2shares.pl
+++ gift-ares/tools/gift2shares.pl
@@ -1,4 +1,22 @@
 #!/usr/bin/perl -w
+
+#
+# $Id: gift2shares.pl,v 1.3 2005/12/17 23:09:58 mkern Exp $
+#
+# Copyright (C) 2005 giFT-Ares project
+# http://developer.berlios.de/projects/gift-ares
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+
 use MIME::Base64;
 
 sub rint { unpack'L',substr $_[0],0,4,'' }
